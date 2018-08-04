ac6b3af493fa188206905453ed28d48e99ab6983
Indu Ramesh <induramesh@gmail.com>
2018-08-04 18:48:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,17 +13,5 @@ const codes = [
 ];
 
 function init() {
-  let index=0
-  document.body.addEventListener('keydown', function(e) {
-  const key= e.key
-  if (key===codes[index]) {
-    index++;
-    if (index===codes.length) {
-      window.alert("GOOD JOB");
-      index=0;
-    }
-    } else {
-     index=0;
-}
-});
+
 }
